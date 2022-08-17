e8f3451b824be66d4cc27a29e701a7de08949069
rajvindergithub <rajvindergithub@gmail.com>
2022-08-17 17:03:29 +0530

    add composer files of mexitek php color

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'mexitek/phpcolors' => array(
+            'pretty_version' => '0.5',
+            'version' => '0.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mexitek/phpcolors',
+            'aliases' => array(),
+            'reference' => '9d7cf28aab475a26b9f5be4f9dc4106736956678',
+            'dev_requirement' => false,
+        ),
+    ),
+);
